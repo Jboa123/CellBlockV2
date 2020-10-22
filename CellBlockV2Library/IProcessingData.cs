3ac0487fe4336feb6a4f10d0819049ee530d8193
Josh Baker <jbaker.8744@gmail.com>
2020-10-22 08:25:49 +0100

    Added processing data class and interface

---
+++ CellBlockV2Library/IProcessingData.cs
@@ -0,0 +1,13 @@
+﻿using CellBlockV2Library.Puzzle_Objects;
+using System.Collections.Generic;
+
+namespace CellBlockV2Library
+{
+    public interface IProcessingData
+    {
+        Dictionary<int, List<List<int>>> BlockDimensionSets { get; set; }
+        IGrid Grid { get; set; }
+        int MaxStackHeight { get; set; }
+        int SolvedMainBLockCount { get; set; }
+    }
+}
