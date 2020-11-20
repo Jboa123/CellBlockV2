e5b51f4de8946121da13ca15919e259602eb73cb
Josh Baker <jbaker.8744@gmail.com>
2020-11-20 01:07:52 +0000

    Updated classes and interfaces - mainblockinstance

---
+++ CellBlockV2Library/Puzzle Objects/IMainBlockInstance.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace CellBlockV2Library.Puzzle_Objects
+{
+    public interface IMainBlockInstance
+    {
+        HashSet<int> PossibleBlocks { get; set; }
+        int SolutionIndex { get; set; }
+    }
+}
---
+++ CellBlockV2Library/Puzzle Objects/MainBlockInstance.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CellBlockV2Library.Puzzle_Objects
+{
+    public class MainBlockInstance : IMainBlockInstance
+    {
+        public HashSet<int> PossibleBlocks { get; set; }
+
+        public int SolutionIndex { get; set; }
+    }
+}
